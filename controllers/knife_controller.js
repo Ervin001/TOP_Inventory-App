6a9f4254a17ad9b95e96a0de18f311212ee3904d
Ervin Gonzalez <83973082+Ervin001@users.noreply.github.com>
2023-12-30 10:59:19 -0600

    Fixed some code in the knife model

--- controllers/knife_controller.js
+++ controllers/knife_controller.js
@@ -10,3 +10,31 @@ exports.index = (req, res) => {
 exports.knife_list = (req, res) => {
   res.send('NOT IMPLEMENTED: Knife list');
 };
+
+exports.knife_detail = (req, res) => {
+  res.send('NOT IMPLEMENTED: Knife detail:');
+};
+
+exports.knife_create_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Knife create GET');
+};
+
+exports.knife_create_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Knife create POST');
+};
+
+exports.knife_delete_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Knife delete GET');
+};
+
+exports.knife_delete_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Knife delete POST');
+};
+
+exports.knife_update_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Knife update GET');
+};
+
+exports.knife_update_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Knife update POST');
+};
--- models/knife.js
+++ models/knife.js
@@ -9,16 +9,54 @@ const KnifeSchema = new Schema({
     type: String,
     required: true,
   },
-  metal_composition: Schema.Types.ObjectId,
-  blade_type: Schema.Types.ObjectId,
-  blade_shape: Schema.Types.ObjectId,
+  metal_composition: {
+    type: Schema.Types.ObjectId,
+    ref: 'Metal',
+    required: true,
+  },
+  blade_shape: {
+    type: Schema.Types.ObjectId,
+    ref: 'BladeShape',
+    required: true,
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  description: String,
 
   price: {
     type: Number,
-    min: [0, 'Price must be positive'],
+    min: [0, 'Price cannot be negative'],
   },
 
-  description: String,
+  number_in_stock: {
+    type: Number,
+    min: [0, 'Cannot have negative inventory'],
+    required: true,
+  },
+});
+
+/*
+name:  Bugout
+metal: s30v
+shape: drop point
+category: folding
+description: a lightweight folder
+price: 120
+number in stock: 5
+*/
+
+// Virtual for knife's URL
+KnifeSchema.virtual('url').get(function () {
+  // This will be needed, cannot use arrow function
+  return `/catalog/knife/${this._id}`;
 });
 
 module.exports = mongoose.model('Knife', KnifeSchema);
+
+/*
+a name, description, category, price, number-in-stock and URL, though you should feel free to add more fields if it seems relevant to the type of business you’ve chosen.
+Categories should at least have a name, a description and a URL.
+*/
